251221299a9fe41b5a31ff78e839f8ed5396aba3
Gene H <gshuh.mail@gmail.com>
2020-06-01 22:28:20 -0400

    Complete calculated average score per exercise

--- src/components/ExercisesList.vue
+++ src/components/ExercisesList.vue
@@ -3,10 +3,11 @@
     <div
       class="exercise"
       :class="{ stripe: i % 2 === 0 }"
-      v-for="(exercise, i) in exercises"
+      v-for="(exercise, i) in exercisesWithScores"
       :key="exercise.id"
     >
-      {{ exercise.name }}
+      <span>{{ exercise.name }}</span>
+      <span>{{exercise.averageScore.toFixed(2)}}</span>
     </div>
   </div>
 </template>
@@ -22,7 +23,33 @@ export default {
   },
   async created() {
     const exerciseService = new ExerciseService();
-    this.exercises = await exerciseService.getAll();
+    const filteredExercises = await exerciseService._getAllIncluding(
+      '?_embed=studentScores'
+    );
+    this.exercises = filteredExercises.map((exercise) => {
+      return {
+        exerciseId: exercise.id,
+        name: exercise.name,
+        scores: exercise.studentScores.map((entry) => {
+          if (entry.score != null) {
+            return entry.score;
+          } else {
+            return 0;
+          }
+        }),
+      };
+    });
+  },
+  computed: {
+    exercisesWithScores() {
+      return this.exercises.map((exercise) => {
+        return {
+          name: exercise.name,
+          averageScore:
+            exercise.scores.reduce((a, b) => a + b, 0) / exercise.scores.length,
+        };
+      });
+    },
   },
 };
 </script>
@@ -30,6 +57,7 @@ export default {
 <style scoped>
 .exercise {
   display: flex;
+  justify-content: space-between;
   border-bottom: 1px solid #add8e6;
   width: 100%;
   padding: 15px;
